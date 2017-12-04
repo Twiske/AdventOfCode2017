b1173b9e40000b2300ffb166e69af7283fa17bab
Loftis, Christopher <Christopher.Loftis@gtri.gatech.edu>
2017-12-04 16:01:37 -0500

    Starting 2-2

---
+++ 2/2-2.py
@@ -0,0 +1,22 @@
+import fileinput
+import re
+import sys
+
+lines = fileinput.input(files=('input'))
+sum = 0
+
+
+for line in lines:
+    min = 1000000
+    max = 0
+
+    for value in list(map(int,line.split("\t"))):
+        max = value if value > max else max
+        min = value if value < min else min
+    sum+= max - min
+
+
+
+print(sum)
+
+
