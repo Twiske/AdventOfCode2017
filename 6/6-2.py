4d708880d8464f199572ae947a239acb96e67c1a
Twiske <twiske@gmail.com>
2017-12-08 01:12:52 -0500

    6-2 down

---
+++ 6/6-2.py
@@ -0,0 +1,38 @@
+import fileinput
+import re
+import sys
+import operator
+import math
+import copy
+
+lines = fileinput.input(files=('input'))
+n = 0
+cycleLength = 0
+
+bankArray = list(map(int,lines[0].split('\t')))
+numBanks = len(bankArray)
+
+bankArrayHistory = []
+bankArrayHistory.append(copy.copy(bankArray))
+
+while True:
+
+	n += 1
+	bank, blocks = max(enumerate(bankArray), key=operator.itemgetter(1))
+
+	allBanks = math.floor(blocks / numBanks)
+	remaining = blocks % numBanks
+	bankArray[bank] = 0
+	bankAray = [x+allBanks for x in bankArray]
+	while remaining > 0:
+		bankArray[(bank + remaining) % numBanks] += 1
+		remaining -= 1
+	if bankArray in bankArrayHistory:
+		cycleLength = n - bankArrayHistory.index(bankArray)
+		break;
+	#print(bankArray)
+	bankArrayHistory.append(copy.copy(bankArray))
+
+print(bankArrayHistory)
+print("iterations: " + str(n))
+print("cycle length: " + str(cycleLength))
