870cbe5d4d7f99c66f08bc37c472e253664eeea4
Twiske <twiske@gmail.com>
2017-12-18 04:52:06 -0500

    10 part 1

--- 10/10-1.py
+++ 10/10-1.py
@@ -10,15 +10,21 @@ lines = fileinput.input(files=('input'))
 lengths = list(map(int,lines[0].split(",")))
 
 skipSize = 0
+totalShift = 0
 
 print(str(lengths))
-knots = list(range(1,255))
-
+knots = list(range(0,256))
+print(knots)
 for length in lengths:
     knots[0:length] = knots[0:length][::-1]
-    shiftAmmount = length + skipSize
-    knots = knots[-shiftAmmount:] + knots[:-shiftmAmount]
+    shiftAmmount = (length + skipSize) % len(knots)
+    knots = knots[shiftAmmount:] + knots[:shiftAmmount]
+    totalShift += shiftAmmount
     skipSize += 1
-
-
+    print("After length: " +  str(length) + " " + str(knots))
+#print(knots)
+totalShift = totalShift % len(knots)
+knots = knots[-totalShift:] + knots[:-totalShift]
+print(totalShift)
 print(knots)
+print(knots[0] * knots[1])
